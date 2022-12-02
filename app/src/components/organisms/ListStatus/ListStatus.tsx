3702c834f210634ccb01ff891097ecf8784dcf81
Romain LEMBO <romainlembo06@gmail.com>
2022-12-02 15:15:35 +0100

    list by status ok

--- app/src/components/organisms/ListStatus/ListStatus.tsx
+++ app/src/components/organisms/ListStatus/ListStatus.tsx
@@ -1,6 +1,6 @@
 import type { Status } from '@/commons'
 
-import { RLList } from '../../atoms'
+import { RLCol, RLList, RLRow } from '../../atoms'
 
 type RLListStatusItem = { name: string; id: string; status: Status }
 
@@ -9,21 +9,39 @@ type RLListStatusProps = {
 }
 
 function RLListStatus({ dataSource }: RLListStatusProps) {
+  const dataSourceByPendingStatus = dataSource.filter(({ status }) => status === 'PENDING')
+  const dataSourceByDoneStatus = dataSource.filter(({ status }) => status === 'DONE')
+  const dataSourceByRejectedStatus = dataSource.filter(({ status }) => status === 'REJECTED')
+
+  const dataSourceMap: { [statusKey: string]: RLListStatusItem[] } = {
+    PENDING: dataSourceByPendingStatus,
+    DONE: dataSourceByDoneStatus,
+    REJECTED: dataSourceByRejectedStatus,
+  }
+  const colSpan: number = 24 / Object.keys(dataSourceMap).length
+
   return (
-    <RLList
-      dataSource={dataSource}
-      renderItem={(item) => (
-        <RLList.Item
-          actions={[<a key='list-loadmore-edit'>edit</a>, <a key='list-loadmore-more'>more</a>]}
-        >
-          <RLList.Item.Meta
-            title={<a href='https://ant.design'>{item.name}</a>}
-            description='Ant Design, a design language for background applications, is refined by Ant UED Team'
+    <RLRow gutter={[16, 16]}>
+      {Object.keys(dataSourceMap).map((statusKey: string) => (
+        <RLCol span={colSpan} key={statusKey}>
+          <RLList
+            dataSource={dataSourceMap[statusKey]}
+            renderItem={({ id, name, status }) => (
+              <RLList.Item
+                key={`${statusKey}-${id}`}
+                actions={[
+                  <a key='list-loadmore-edit'>edit</a>,
+                  <a key='list-loadmore-more'>more</a>,
+                ]}
+              >
+                <RLList.Item.Meta title={<a href='https://ant.design'>{name}</a>} />
+                <div>{status}</div>
+              </RLList.Item>
+            )}
           />
-          <div>{item.status}</div>
-        </RLList.Item>
-      )}
-    />
+        </RLCol>
+      ))}
+    </RLRow>
   )
 }
 
